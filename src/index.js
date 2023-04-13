43c1f36596a390a2869d39605917bd049216e884
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-04-13 11:45:57 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs/promises");
+const fileName = "myfile.txt";
+const reader = async (fileName) => {
+  // write code here
+	// dont change function name
+  
+};
+
+
+module.exports =  reader ;
